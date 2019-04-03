fa0338fbee6ab7bbb790506a740e2c93c35d426c
boutaleb <you@example.com>
2019-04-04 00:06:59 +0200

    first commit

---
+++ src/main/java/com/example/demo/ServerGestionProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionProduitApplication.class, args);
+	}
+
+}
